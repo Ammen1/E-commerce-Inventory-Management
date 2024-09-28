42f903ed3a052a9a7c34058df4787599302b1d2c
Tamirat <amenguda@gmail.com>
2024-09-29 02:53:49 +0300

    add InventoryItem model with validation and indexes

--- models/inventoryItem.Model.js
+++ models/inventoryItem.Model.js
@@ -38,10 +38,6 @@ const inventoryItemSchema = new mongoose.Schema({
     default: 10,
     min: [0, 'Low stock threshold must be at least 0'],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
@@ -53,7 +49,7 @@ const inventoryItemSchema = new mongoose.Schema({
 inventoryItemSchema.index({ name: 1 });
 inventoryItemSchema.index({ category: 1, price: -1 });
 
-// Add a custom validation method to ensure that quantity is always above the lowStockThreshold when quantity is updated
+
 inventoryItemSchema.pre('save', function (next) {
   if (this.quantity < this.lowStockThreshold) {
     return next(new Error('Quantity cannot be less than low stock threshold.'));
