e19ea0a5ffa572236c114d6f33cc1a41c1a09bcc
Tamirat <amenguda@gmail.com>
2024-09-28 18:05:18 +0300

    add mothed to generate a JWT token, to hash password and Method to comper the entered password

---
+++ models/user.Model.js
@@ -0,0 +1,96 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+    minlength: [2, 'Name must be at least 2 characters long'],
+    maxlength: [50, 'Name cannot exceed 50 characters'],
+  },
+  phone: {
+    type: String,
+    required: [true, 'Phone number is required'],
+    set: (v) => v.replace(/\D/g, ''),
+    validate: {
+      validator: (v) => /^[\d]{10}$/.test(v),
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [8, 'Password must be at least 8 characters long'],
+    maxlength: [32, 'Password must be less than or equal to 32 characters long'],
+  },
+  role: {
+    type: String,
+    enum: ['Admin', 'Manager', 'Employee'],
+    default: 'Employee',
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  passwordResetToken: {
+    type: String,
+  },
+  passwordResetExpires: {
+    type: Date,
+  },
+}, { timestamps: true });
+
+
+userSchema.index({ email: 1 });
+userSchema.index({ role: 1, isActive: 1, createdAt: -1 });
+
+// Pre-save hook to hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare the entered password with the hashed password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Method to generate a JWT token
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: '1h',
+  });
+};
+
+// Method to remove sensitive fields from the user object when converting to JSON
+userSchema.methods.toJSON = function () {
+  const userObject = this.toObject();
+  delete userObject.password;
+  delete userObject.passwordResetToken;
+  delete userObject.passwordResetExpires;
+  return userObject;
+};
+
+
+const User = mongoose.model('User', userSchema);
+export default User;
